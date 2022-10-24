eb792f408c98b1fa2052c8af23f49e3a5f1a14a6
Paata Home <paata.tepnadze@gmail.com>
2022-10-24 20:35:55 +0200

    state properties

--- restl006/src/Component/button.jsx
+++ restl006/src/Component/button.jsx
@@ -4,30 +4,48 @@ class Button extends Component {
 
     constructor(props){
         super(props);
-        this.handlerMe = this.handlerclick;
+        this.state = {
+            counter : 0
+        }
+     //   this.handlerMe = this.handlerclick;
     }
 
-    Handler(event){
+  /*   Handler(event){
         console.log(this, event.target);
-    }
+    } */
 
     handlerclick(event){
-        let name =event.target;
-        console.log(event.nativeEvent.toElement);
+        this.setState({counter:++this.state.counter});
+        /* let name =event.target;
+        console.log(event.nativeEvent.toElement); */
     }
     
     render() { 
         return (
             <div className='App'>
-                <div className="child">
+                <Buttonclick handleMe={this.handlerclick.bind(this)}
+                 counter = {this.state.counter}/>
+               {/*  <div className="child">
                     <p onMouseOver= { (e)=>{
                         console.log(e.target);
                     }} >Hover Me</p>
-                </div>
-                <button className='btn btn-warning' onClick={this.handlerMe}>Save</button>
+                </div> */}
+                {/* <button className='btn btn-primary' onClick={this.handlerclick.bind(this)}>Increase Vaue By Clicking ME {this.state.counter}</button> */}
             </div>
         );
     }
 }
  
 export default Button;
+
+
+let Buttonclick = (props) =>{
+    return(
+            <button 
+            className='btn btn-primary' 
+            onClick={props.handleMe}>
+                Increase Vaue By Clicking ME 
+                {props.counter}
+            </button>
+    );
+}
